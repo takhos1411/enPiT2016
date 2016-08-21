1af8a8d382f54d04cad03eeaeeb18718008b41ed
takhos1411 <takhos1411@gmail.com>
2016-08-21 22:59:56 +0900

    X Implementing View Routing

---
+++ js/EmployeeView.js
@@ -0,0 +1,13 @@
+var EmployeeView = function(employee) {
+	
+	this.initialize = function() {
+		this.$el = $('<div/>');
+	};
+
+	this.render = function() {
+		this.$el.html(this.template(employee));
+		return this;
+	};
+
+	this.initialize();
+}
