6e5d8475bd53546e2adf31fc07f63cd51e784c3a
takhos1411 <takhos1411@gmail.com>
2016-08-21 17:14:24 +0900

    VIII. Using Handlebars Templates

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 (function () {
 
     /* ---------------------------------- Local Variables ---------------------------------- */
+	var homeTpl = Handlebars.compile($("#home-tpl").html()); 
+	var employeeListTpl = Handlebars.compile($("#employee-list-tpl").html()); 
     var service = new EmployeeService();
     service.initialize().done(function () {
 		renderHomeView();
@@ -33,6 +35,7 @@
     /* ---------------------------------- Local Functions ---------------------------------- */
     function findByName() {
         service.findByName($('.search-key').val()).done(function (employees) {
+/*
             var l = employees.length;
             var e;
             $('.employee-list').empty();
@@ -40,15 +43,20 @@
                 e = employees[i];
                 $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
             }
+*/
+			 $('.content').html(employeeListTpl(employees)); 
         });
     }
 
 	function renderHomeView() {
+/*
 		var html =
 			"<h1>Directory</h1>" +
 			"<input class='search-key' type='search' placeholder='Enter name'/>" +
 			"<ul class='employee-list'></ul>";
 		$('body').html(html);
+*/
+		$('body').html(homeTpl()); 
 		$('.search-key').on('keyup', findByName);
 	}
 
